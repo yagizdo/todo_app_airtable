7887e410d80df333f2d5bf13e9e90d2afe03e380
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-07-24 20:12:13 +0300

    added floating action button

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:todo_app_airtable/core/network_service.dart';
 import 'package:todo_app_airtable/widget/todo_builder.dart';
 
@@ -21,6 +22,30 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: SizedBox(
+        height: 80.w,
+        width: 80.w,
+        child: FloatingActionButton(
+          elevation: 10,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(20),
+            ),
+          ),
+          onPressed: () {},
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon(Icons.add),
+              Text('Add Todo'),
+            ],
+          ),
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text('Todos'),
+      ),
       body: TodoBuilder(
         future: getTodos,
       ),
