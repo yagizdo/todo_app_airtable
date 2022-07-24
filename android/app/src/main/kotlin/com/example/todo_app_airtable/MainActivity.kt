d42439eb8d9ee42f2abde71c01fa299f345a3d9d
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-07-24 17:52:25 +0300

    app init

---
+++ android/app/src/main/kotlin/com/example/todo_app_airtable/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todo_app_airtable
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app_airtable/view/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Material App',
+      home: HomeView(),
+    );
+  }
+}
