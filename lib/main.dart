82c5db9ea90fcaf0226c8697329a387af6440742
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-07-24 18:18:31 +0300

    .env init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:todo_app_airtable/view/home_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // env
+  await dotenv.load(fileName: ".env");
+
   runApp(const MyApp());
 }
 
