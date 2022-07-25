26e3dc8e871bee6a7835e094b6430794e3e05a5f
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-07-25 16:00:36 +0300

    added loading state check

--- lib/widget/todo_builder.dart
+++ lib/widget/todo_builder.dart
@@ -1,15 +1,30 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app_airtable/bloc/todo_bloc.dart';
 import 'package:todo_app_airtable/widget/todo_list.dart';
 
-class TodoBuilder extends StatelessWidget {
+class TodoBuilder extends StatefulWidget {
   const TodoBuilder({Key? key}) : super(key: key);
+
+  @override
+  State<TodoBuilder> createState() => _TodoBuilderState();
+}
+
+class _TodoBuilderState extends State<TodoBuilder> {
+  bool isLoading = true;
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<TodoBloc, TodoState>(
       builder: (context, state) {
-        if (state is LoadingState) {
+        if (state is LoadingState && isLoading == true) {
+          // If data not loading set isLoading false
+          Timer.periodic(const Duration(seconds: 5), (timer) {
+            setState(() {
+              isLoading = false;
+            });
+          });
           return const Center(
             child: CircularProgressIndicator(
               color: Colors.white,
